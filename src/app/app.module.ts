3ab1bce289bcdb5a1e82c4ad157db3aa194163a8
AbdullahNasir <abdullah555@yahoo.com>
2017-01-16 15:19:57 +0500

    initial commit with basic search form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,23 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { MyDatePickerModule } from 'mydatepicker';
+
+
 import { AppComponent } from './app.component';
+import { SearchComponentComponent } from './search-component/search-component.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponentComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MyDatePickerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/search-component/search-component.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'search-component',
+  templateUrl: './search-component.component.html',
+  styleUrls: ['./search-component.component.css']
+})
+export class SearchComponentComponent implements OnInit {
+
+  private today:Object;
+
+  constructor() {
+    this.today = {year: 2017, month: 1, day: 4}
+  }
+
+  ngOnInit() {
+  }
+
+  onDateChanged($event){
+    console.log(this.today);
+  }
+
+}
