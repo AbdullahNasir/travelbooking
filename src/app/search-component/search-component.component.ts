6aa32388962f75bbb032073804a411de7d2e3ac0
AbdullahNasir <abdullah555@yahoo.com>
2017-01-18 16:35:26 +0500

    fixed responsiveness issues

--- src/app/search-component/search-component.component.ts
+++ src/app/search-component/search-component.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { Booking } from '../classes/booking';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/of';
-// import { CompleterService, CompleterData } from 'ng2-completer';
 
 @Component({
   selector: 'search-component',
@@ -15,16 +14,7 @@ export class SearchComponentComponent implements OnInit {
   private origin: String;
 
   private searchStr: string;
-  // private dataService : String;
-  // private searchData = [
-  //   { color: 'red', value: '#f00' },
-  //   { color: 'green', value: '#0f0' },
-  //   { color: 'blue', value: '#00f' },
-  //   { color: 'cyan', value: '#0ff' },
-  //   { color: 'magenta', value: '#f0f' },
-  //   { color: 'yellow', value: '#ff0' },
-  //   { color: 'black', value: '#000' }
-  // ];
+  
   private searchData = {
     color : ['red','green','blue','cyan','magenta','yellow','black'],
     value : ['#f00' ,'#0f0','#00f','#fof','ff0','#000']
